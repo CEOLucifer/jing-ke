caf033d852b48dd90a4596936488da03d5561cd9
Xujunyu <1619867043@qq.com>
2026-05-06 20:38:22 +0800

    城墙建筑

--- script/ui/ChatPanel.cs
+++ script/ui/ChatPanel.cs
@@ -3,24 +3,24 @@ using System;
 
 public partial class ChatPanel : Control
 {
-    [Export] public TextEdit textEdit;
-    [Export] public Button btnSend;
+	[Export] public TextEdit textEdit;
+	[Export] public Button btnSend;
 
-    [Export]
-    public Agent agent;
+	[Export]
+	public Agent agent;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        btnSend.Pressed += () =>
-        {
-            var content = textEdit.Text;
-            agent.Send(content, (res) =>
-            {
-                GD.Print(res);
-            });
-            textEdit.Clear();
-        };
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		btnSend.Pressed += () =>
+		{
+			var content = textEdit.Text;
+			agent.Send(content, (res) =>
+			{
+				GD.Print(res);
+			});
+			textEdit.Clear();
+		};
+	}
 
 }
